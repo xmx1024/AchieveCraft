b26a91bebc64a9461c0a4be29b148088ad188e56
Spencer Hayden <xmx1024@gmail.com>
2016-01-12 10:45:44 -0700

    update index.php
    Changed some grammatical errors for you

--- pages/index.php
+++ pages/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-    <title>AchieveCraft - Make Minecraft achievement images</title>
+    <title>AchieveCraft - Make Minecraft Achievement Images</title>
 
     <!-- CSS  -->
     <link href="css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
